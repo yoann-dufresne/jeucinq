047ac2b9c97803eeb10b39a8009a482606455d0c
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-12-01 17:43:43 +0100

    main.cpp server

---
+++ ESP Server/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <WifiManager.hpp>
+
+ServerWifiManager server;
+
+void setup() {
+  server.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
